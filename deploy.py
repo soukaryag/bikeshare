637419119423d5c0212d7fd3904ebf813c05f785
soukaryag <sg4fz@virginia.edu>
2018-11-04 03:38:38 -0500

    comments added to various files

--- deploy.py
+++ deploy.py
@@ -1,5 +1,5 @@
 ## PROPERTY OF SOUKARYA GHOSH ##
-
+## MAIN FILE FOR THE PROGRAM, CALLS ALL MAJOR METHODS AND INITIATES THE FLASK APP WHILE CALLING HTML FILES ##
 
 from flask import Flask
 from flask import Blueprint, render_template
